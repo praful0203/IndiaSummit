776077d9f1a86b28cd2a37e8c0a990202ae4551d
Praful Mishra <prafulmishra0203@gmail.com>
2017-10-16 21:32:38 +0530

    Navigation Drawer with About us details added

--- app/src/main/java/com/prafulmishra/indiasummit/Splashscreen.java
+++ app/src/main/java/com/prafulmishra/indiasummit/Splashscreen.java
@@ -46,7 +46,7 @@ public class Splashscreen extends Activity {
                 try {
                     int waited = 0;
                     // Splash screen pause time
-                    while (waited < 6000) {
+                    while (waited < 4500) {
                         sleep(100);
                         waited += 100;
                     }
