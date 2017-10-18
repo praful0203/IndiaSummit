f8cddaa8218f085797f21f10f0552f9f08d65b88
Praful Mishra <prafulmishra0203@gmail.com>
2017-10-18 21:40:51 +0530

    Added database connectivity to Participant Registration

--- app/src/main/java/com/prafulmishra/indiasummit/ParticipantActivity.java
+++ app/src/main/java/com/prafulmishra/indiasummit/ParticipantActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,17 +18,28 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.prafulmishra.indiasummit.data.Participant;
+
 public class ParticipantActivity extends AppCompatActivity {
+
     Button btnApply;
     EditText partpntName,txtCity,txtCol_org,txtMob,txtMail,txtExpect,txtEvent,txtAltitude;
     RatingBar rtTech,rtLead;
     RadioGroup rdGroup;
-    RadioButton rbtnYes,rdbtnMaybe,rdbtnNo;
+    RadioButton rb;
+    String name,city,college_org,mobile,mailid,expect,events_attended,next_5years,attend_event;
+    Float tech_skills,lead_skills;
+
+   DatabaseReference databaseParticipant;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_participant);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        databaseParticipant = FirebaseDatabase.getInstance().getReference("participants");
 
         partpntName = (EditText)findViewById(R.id.partpntName);
         txtCity = (EditText)findViewById(R.id.txtCity);
@@ -39,9 +51,7 @@ public class ParticipantActivity extends AppCompatActivity {
         txtAltitude = (EditText)findViewById(R.id.txtAltitude);
 
         rdGroup = (RadioGroup)findViewById(R.id.rdGroup);
-        rbtnYes = (RadioButton)findViewById(R.id.rbtnYes);
-        rdbtnMaybe = (RadioButton)findViewById(R.id.rdbtnMaybe);
-        rdbtnNo = (RadioButton)findViewById(R.id.rdbtnNo);
+
 
         rtLead = (RatingBar)findViewById(R.id.rtLead);
         rtTech = (RatingBar)findViewById(R.id.rtTech);
@@ -52,7 +62,8 @@ public class ParticipantActivity extends AppCompatActivity {
         btnApply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(ParticipantActivity.this, "Rating :"+rtTech.getRating(), Toast.LENGTH_SHORT).show();
+                getTextData();
+               // Toast.makeText(ParticipantActivity.this, name+" "+city+" "+college_org+" "+mobile+" "+mailid+" "+expect+" "+events_attended+" "+next_5years+" "+attend_event+" "+tech_skills+" "+lead_skills , Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -64,6 +75,64 @@ public class ParticipantActivity extends AppCompatActivity {
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    private void getTextData() {
+        name = partpntName.getText().toString();
+        city = txtCity.getText().toString();
+        college_org = txtCol_org.getText().toString();
+        mobile = txtMob.getText().toString();
+        mailid = txtMail.getText().toString();
+        expect = txtExpect.getText().toString();
+        events_attended = txtEvent.getText().toString();
+        next_5years = txtAltitude.getText().toString();
+        tech_skills = rtTech.getRating();
+        lead_skills = rtLead.getRating();
+        String category = "Participant";
+
+        boolean failFlag = false;
+
+        if( name.length() == 0 )
+        {
+            failFlag = true;
+            partpntName.setError( "Field cannot be empty" );
+        }
+        if( city.trim().length() == 0 )
+        {
+            failFlag = true;
+            txtCity.setError( "Field cannot be empty" );
+        }
+        if( college_org.length() == 0 )
+        {
+            failFlag = true;
+            txtCol_org.setError( "Field cannot be empty" );
+        }
+        if( mobile.length() == 0 )
+        {
+            failFlag = true;
+            txtMob.setError( "Field cannot be empty" );
+        }
+        if( mailid.length() == 0 )
+        {
+            failFlag = true;
+            txtMail.setError( "Field cannot be empty" );
+        }
+        if( expect.length() == 0 )
+        {
+            failFlag = true;
+            txtExpect.setError( "Field cannot be empty" );
+        }
+        if( next_5years.length() == 0 )
+        {
+            failFlag = true;
+            txtAltitude.setError( "Field cannot be empty" );
+        }
+        // if all are fine
+        if (!failFlag) {
+           String id = databaseParticipant.push().getKey();
+            Participant participant = new Participant(name,city,college_org,mobile,mailid,expect,events_attended,tech_skills,lead_skills,next_5years,attend_event);
+            databaseParticipant.child(id).setValue(participant);
+        }
     }
 
     private void showInfo() {
@@ -92,5 +161,12 @@ public class ParticipantActivity extends AppCompatActivity {
         // Showing Alert Message
         alertDialog.show();
 
+    }
+
+    public void rbClick(View v)
+    {
+        int rbId = rdGroup.getCheckedRadioButtonId();
+        rb = (RadioButton)findViewById(rbId);
+        attend_event = rb.getText().toString();
     }
 }
