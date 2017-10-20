9c711cc8c81d859bebb6bb5b7addc1f03eb579b8
Praful Mishra <prafulmishra0203@gmail.com>
2017-10-20 21:01:10 +0530

    Added email validations

--- app/src/main/java/com/prafulmishra/indiasummit/ContactUs.java
+++ app/src/main/java/com/prafulmishra/indiasummit/ContactUs.java
@@ -1,13 +1,97 @@
 package com.prafulmishra.indiasummit;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ContactUs extends AppCompatActivity {
-
+    Button btnSubmit;
+    EditText name,mail,subject,message;
+    String ctname,ctmail,ctsub,ctmsg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_us);
+
+        btnSubmit = (Button)findViewById(R.id.btnSubmit);
+
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                name = (EditText)findViewById(R.id.contactName);
+                mail = (EditText)findViewById(R.id.contactMail);
+                subject = (EditText)findViewById(R.id.contactSub);
+                message = (EditText)findViewById(R.id.contactMsg);
+
+
+                ctname = name.getText().toString();
+                ctmail = mail.getText().toString();
+                ctsub = subject.getText().toString();
+                ctmsg = message.getText().toString();
+
+                boolean failFlag = false;
+
+                if( ctname.length() == 0 )
+                {
+                    failFlag = true;
+                    name.setError( "Field cannot be empty" );
+                    name.requestFocus();
+                }
+                if( ctsub.trim().length() == 0 )
+                {
+                    failFlag = true;
+                    subject.setError( "Field cannot be empty" );
+                    subject.requestFocus();
+                }
+                if(!isValidEmail(ctmail))
+                {
+                        failFlag = true;
+                        mail.setError( "Enter a valid mail ID" );
+                        mail.requestFocus();
+
+                }
+                if( ctmsg.length() == 0 )
+                {
+                    failFlag = true;
+                    message.setError( "Field cannot be empty" );
+                    message.requestFocus();
+                }
+
+                // if all are fine
+                if (!failFlag) {
+                    Intent sendEmail = new Intent(Intent.ACTION_SEND);
+                    sendEmail.setData(Uri.parse("mailto:"));
+                     sendEmail.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"girlscriptfoundation@gmail.com","anubha.girlscript@gmail.com"});
+                    sendEmail.putExtra(android.content.Intent.EXTRA_SUBJECT,""+ctsub);
+                    sendEmail.putExtra(android.content.Intent.EXTRA_TEXT,
+                            "Name:"+ctname+"\n"+"Email ID:"+ctmail+"\n"+"Subject: "+ctsub+"\n"+"Message:"+'\n'+ctmsg);
+                    sendEmail.setType("message/rfc2822");
+            /* Send it off to the Activity-Chooser */
+                    startActivity(Intent.createChooser(sendEmail, "Send mail..."));
+                    Intent chooser = Intent.createChooser(sendEmail,"Send Email");
+                    Toast.makeText(ContactUs.this, R.string.select_gmail, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+
+    // validating email id
+
+    public final static boolean isValidEmail(CharSequence target) {
+        if (target == null) {
+            return false;
+        } else {
+            return android.util.Patterns.EMAIL_ADDRESS.matcher(target).matches();
+        }
     }
 }
